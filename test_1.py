8ffa9bd797108863de8b60aa4b65e8d90df6ba00
pranav7712 <ptulshyan77@gmail.com>
2021-11-14 08:18:18 +0530

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -2,9 +2,9 @@ from fyers_api import accessToken
 
 from fyers_api import fyersModel
 
-app_id = "1XVY7MKYZV"
+app_id = "V"
 
-app_secret = "L981CZ96V7"
+app_secret = ""
 
 app_session = accessToken.SessionModel(app_id, app_secret)
 
